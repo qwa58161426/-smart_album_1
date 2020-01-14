80d73068a2edd21dcc749eb928244ffc22be43f1
yangqianshu <1015209395@qq.com>
2020-01-14 11:54:56 +0800

    标签分类:地标

---
+++ src/main/java/com/zhitu/dao/mapper/TagMapper.java
@@ -0,0 +1,25 @@
+package com.zhitu.dao.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 标签映射器
+ */
+public interface TagMapper {
+
+    //在方法参数的前面写上@Param("参数名"),表示给参数命名,名称就是括号中的内容
+    void insert(@Param("name") String name);
+
+    Integer selectExistByName(@Param("name") String name);
+
+    Integer selectTagIdByName(@Param("name") String name);
+
+    List<Integer> selectTagIdLikeName(List<String> keywordList);
+
+    List<Map<String,Object>> selectTags();
+
+    String selectNameByTagId(@Param("tagId") int tagId);
+}
